f6199653153a202c43f2fe80217aa4a33a550bf8
is6769 <igor.shvets2007@mail.ru>
2025-04-04 13:41:10 +0300

    first commit

---
+++ src/main/java/org/example/hrsservice/HrsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.hrsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrsServiceApplication.class, args);
+	}
+
+}

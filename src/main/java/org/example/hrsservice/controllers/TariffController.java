adb70e9398f317a444dc57b73421985df068fd8e
is6769 <igor.shvets2007@mail.ru>
2025-04-21 00:55:35 +0300

    minor changes in hrsservice

--- src/main/java/org/example/hrsservice/controllers/TariffController.java
+++ src/main/java/org/example/hrsservice/controllers/TariffController.java
@@ -1,4 +1,20 @@
 package org.example.hrsservice.controllers;
 
+import org.example.hrsservice.services.TariffService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class TariffController {
+
+    private final TariffService tariffService;
+
+    public TariffController(TariffService tariffService) {
+        this.tariffService = tariffService;
+    }
+
+    @PostMapping
+    public String tarificateCDR(){
+        return "Hello, World";
+    }
 }
---
+++ src/main/java/org/example/hrsservice/services/TariffService.java
@@ -0,0 +1,9 @@
+package org.example.hrsservice.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TariffService {
+
+
+}
